3674bd40c2923727a56b073a8c5963c470d3f172
David Naber <kontakt@dnaber.de>
2016-03-11 13:26:42 +0100

    Add test body for WpDbAdapter

---
+++ tests/phpunit/WpIntegration/Db/WpDbAdapterTest.php
@@ -0,0 +1,44 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace WpDbTools\Db;
+
+use
+	WP_UnitTestCase;
+
+/**
+ * Class WpDbAdapterTest
+ *
+ * @package WpDbTools\Db
+ */
+class WpDbAdapterTest extends WP_UnitTestCase {
+
+	public function test_query() {
+
+		$this->markTestIncomplete( 'Under construction…' );
+	}
+
+	public function test_query_statement() {
+
+		$this->markTestIncomplete( 'Under construction…' );
+	}
+
+	public function test_execute_statement() {
+
+		$this->markTestIncomplete( 'Under construction…' );
+	}
+
+	public function test_last_insert_id() {
+
+		$this->markTestIncomplete( 'Under construction…' );
+	}
+
+	public function test_last_affected_rows() {
+
+		$this->markTestIncomplete( 'Under construction…' );
+	}
+
+	public function test_esc_string() {
+
+		$this->markTestIncomplete( 'Under construction…' );
+	}
+}
